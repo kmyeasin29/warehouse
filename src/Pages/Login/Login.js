b9c929d9b56aaf3723c6ecaf33d097d0636854f5
kmyeasin <kmyeasin29>
2022-05-06 21:47:58 +0600

    Login Form style done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const emailRef=useRef('');
+    const passwordRef=useRef('');
+
+    const handleLogin=event=>{
+        event.preventDefault();
+        const email=emailRef.current.value;
+        const password=passwordRef.current.value;
+        console.log(email, password);
+    }
     return (
         <div className='w-50 mx-auto p-2'>
             <h2 className='text-center text-danger mt-2 display-4'>Login</h2>
-            <Form className='container border'>
+            <Form className='container border' onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -17,15 +26,15 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="danger" type="submit">
+                <Button className='w-100' variant="danger" type="submit">
                     Login
                 </Button>
-                <p className='mt-3 fs-5'>Don't Register Yet??? <Link className='text-decoration-none' to='/Register'>Register Now</Link></p>
+                <p className='mt-3 fs-5'>New to Car Leader??? <Link className='text-decoration-none pe-auto text-danger' to='/Register'>Register Now</Link></p>
             </Form>
         </div>
     );
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const emailRef=useRef('');
+    const passwordRef=useRef('');
+    const confirmPasswordRef=useRef('');
+
+    const handleRegister=event=>{
+        event.preventDefault();
+        const email=emailRef.current.value;
+        const password=passwordRef.current.value;
+        const confirmPassword=confirmPasswordRef.current.value;
+        console.log(email, password, confirmPassword);
+
+    }
     return (
         <div className='w-50 mx-auto p-2'>
         <h2 className='text-center text-danger mt-2 display-4'>Register</h2>
-        <Form className='container border'>
+        <Form className='container border' onSubmit={handleRegister}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                 </Form.Text>
@@ -17,19 +29,19 @@ const Register = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm Password" />
+                <Form.Control ref={confirmPasswordRef} type="password" placeholder="Confirm Password" required/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-            <Button variant="danger" type="submit">
+            <Button className='w-100' variant="danger" type="submit">
             Register
             </Button>
-            <p className='mt-3 fs-5'>Already Registered??? <Link className='text-decoration-none' to='/Login'>Login</Link></p>
+            <p className='mt-3 fs-5'>Already Registered??? <Link className='text-decoration-none pe-auto text-danger' to='/Login'>Login</Link></p>
         </Form>
     </div>
     );
